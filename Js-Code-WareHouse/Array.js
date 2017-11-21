f3eba1c280e48ccd2f692aaffc86d6dde875d5d9
wangwq <wangwq@wangweiqiangdeMacBook-Pro.local>
2017-11-21 17:05:15 +0800

    封装代码收藏

--- Js-Code-WareHouse/Array.js
+++ Js-Code-WareHouse/Array.js
@@ -53,7 +53,7 @@ function removeClass (ele,cls){
     }
 }
 
-Cookie
+三.Cookie
 1.getCookie
 /**
  * @desc 根据name读取cookie
@@ -92,4 +92,57 @@ function setCookie(name,value,days){
     var date = new Date();
     data.setDate(date.getDate() + days);
     document.cookie = name + '=' + value + ';expires=' + date;
+}
+
+四.Device
+4.1.getExplore
+/**
+ * @desc 获取浏览器类型和版本
+ * @return {String}
+ */
+function getExplore(){
+    var sys = {},
+    ua = navigator.userAgent.toLowerCase(),
+    s;
+    (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? sys.ie = s[1]:
+    (s = ua.match(/msie ([\d\.]+)/)) ? sys.ie = s[1] :
+    (s = ua.match(/edge\/([\d\.]+)/)) ? sys.edge = s[1] :
+    (s = ua.match(/firefox\/([\d\.]+)/)) ? sys.firefox = s[1] :
+    (s = ua.match(/(?:opera|opr).([\d\.]+)/)) ? sys.opera = s[1] :
+    (s = ua.match(/chrome\/([\d\.]+)/)) ? sys.chrome = s[1] :
+    (s = ua.match(/version\/([\d\.]+).*safari/)) ? sys.safari = s[1] : 0;
+    // 根据关系进行判断
+    if (sys.ie) return ('IE: ' + sys.ie)
+    if (sys.edge) return ('EDGE: ' + sys.edge)
+    if (sys.firefox) return ('Firefox: ' + sys.firefox)
+    if (sys.chrome) return ('Chrome: ' + sys.chrome)
+    if (sys.opera) return ('Opera: ' + sys.opera)
+    if (sys.safari) return ('Safari: ' + sys.safari)
+    return 'Unkonwn'
+}
+4.2 getOs
+/**
+ * @desc 获取操作系统类型
+ * @return {String}
+ */
+function getOs(){
+    var userAgent = 'navigator' in window && 'userAgent' in navigator && navigator.userAgent.toLowerCase() || '';
+    var vendor = 'navigator' in window && 'vendor' in navigator && navigator.vendor.toLowerCase() || '';
+    var appVersion = 'navigator' in window && 'appVersion' in navigator && navigator.appVersion.toLowerCase() || '';
+
+    if (/mac/i.test(appVersion)) return 'MacOSX'
+    if (/win/i.test(appVersion)) return 'windows'
+    if (/linux/i.test(appVersion)) return 'linux'
+    if (/iphone/i.test(userAgent) || /ipad/i.test(userAgent) || /ipod/i.test(userAgent)) 'ios'
+    if (/android/i.test(userAgent)) return 'android'
+    if (/win/i.test(appVersion) && /phone/i.test(userAgent)) return 'windowsPhone'
+}
+
+五.Dom
+5.1 getScrollTop
+/**
+ * @desc 获取滚动条距顶部的距离
+ */
+function getScrollTop(){
+    return ( document.documentElement && document.documentElement.scrollTo ) || document.body.scrollTop;
 }
